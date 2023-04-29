b4fe5c43c6797e99cd34774d43fbd62cb2c0a0e8
Nicolas Largueze <nick@nicklabs.io>
2023-04-29 14:53:05 +0200

    fix: reupload of v0.2.0

--- Formula/http-intercept.rb
+++ Formula/http-intercept.rb
@@ -5,7 +5,7 @@ class HttpIntercept < Formula
     desc "Simple HTTP request interceptor that echoes the request to the terminal"
     homepage "https://github.com/nlargueze/http-intercept"
     url "https://github.com/nlargueze/http-intercept/releases/download/v0.2.0/http-intercept-mac.tar.gz"
-    sha256 "aea260991bd8ed316e21290f941044077704d52031f1ef70a7afd6d44fe526c8"
+    sha256 "6f5eb21c56032e1ecb03df829f00e237f6c7df59c672924ab410fbd0c80d502b"
     version "0.2.0"
   
     def install
